603e3cec4a7c9896349577d2a8bb950f35070cfa
Linnéa Smeds <linnea.smeds@gmail.com>
2022-10-26 15:48:39 +0200

    Updated plot script S4 and all figures after revision

--- R/plot_figS4.R
+++ R/plot_figS4.R
@@ -1,8 +1,13 @@
 #################
-### Plotting Figure S4:
-### GERP scores distribution for deleterious, synonymous and nonsense!
+### PLOTTING GERP SCORE DISTRIBUTIONS FOR ALL CLASSES OF VEP, AND GENOME WIDE
 
-# Load libraries
+
+#Clear the workspace
+rm(list=ls())
+
+###############################################################################
+# Make DENSITY plot of synonymous and deleterious in the same figure
+# (on top of eachother)
 require(tidyverse)
 
 rm(list=ls())
@@ -10,56 +15,50 @@ plotdir="plots/"
 filt="mac2"
 anc=paste("Pol.2out.",filt, sep="")
 
-
-###############################################################################
-# Make DENSITY plot of synonymous and deleterious in the same figure
-# (on top of eachother)
-
 #Input files
 all_gerp_file=paste("gerp/canFam3/",anc,"/100S95F14R.chr1-38.allSNPs.rates.unique.bed", sep="")
 vep_gerp_file=paste("gerp/canFam3/",anc,"/100S95F14R.chr1-38.vepfinal.rates.unique.bed", sep="")
+mod_gerp_file=paste("gerp/canFam3/",anc,"/100S95F14R.chr1-38.modifier.rates.unique.bed", sep="")
 vep_file=paste("vep/",anc,"/veptypes.chr1-X.txt", sep="")
 sift_file=paste("vep/",anc,"/sifttypes.chr1-X.txt", sep="")
 
-all_gerp_tib <- all_gerp_file %>% read.table(header=FALSE) %>% as_tibble() %>%
+allgerp_tib <- all_gerp_file %>% read.table(header=FALSE) %>% as_tibble() %>%
                 rename(CHROM=V1, POS=V3, GERP=V5) %>%
                 mutate(CHROM = as.character(CHROM))
-vep_gerp_tib<-vep_gerp_file %>% read.table(header=FALSE) %>% as_tibble() %>% rename(CHROM=V1, POS=V3, GERP=V5) %>%
+gerp_tib<-vep_gerp_file %>% read.table(header=FALSE) %>% as_tibble() %>% rename(CHROM=V1, POS=V3, GERP=V5) %>%
                 mutate(CHROM = as.character(CHROM))
+mod_tib<-mod_gerp_file %>% read.table(header=FALSE) %>% as_tibble() %>% rename(CHROM=V1, POS=V3, GERP=V5) %>%
+                  mutate(CHROM = as.character(CHROM))
 vep_tib <- vep_file %>% read.table(header=TRUE) %>% as_tibble() %>% mutate(CHROM = as.character(CHROM))
 sift_tib <- sift_file %>% read.table(header=TRUE) %>% as_tibble() %>% mutate(CHROM = as.character(CHROM))
 
-coding_tib <- vep_gerp_tib %>% inner_join(vep_tib) %>% left_join(sift_tib) %>%
-      rename(Type=VEP_TYPE) %>% filter(Type=='synonymous' |Type=='nonsense' | SIFT_TYPE == 'deleterious') %>%
-      mutate(Type=ifelse(Type=='missense', 'deleterious',Type)) %>%
-      mutate(Region="Coding") %>%
+comb_tib<-gerp_tib %>% add_row(mod_tib)
+
+tidy_tab <- comb_tib %>% inner_join(vep_tib) %>% left_join(sift_tib) %>%
+      rename(Type=VEP_TYPE) %>%
+      mutate(Type=case_when((SIFT_TYPE=="deleterious" | SIFT_TYPE=="tolerated") ~ SIFT_TYPE,
+                      (is.na(Type) ~ 'modifier'),
+                      TRUE ~ Type)) %>%
+      mutate(Region="Genic") %>%
       select(CHROM, POS, GERP, Type, Region)
 
-wg_tib <- all_gerp_tib  %>% mutate(Type="all", Region="WholeGenome") %>%
+tidy_full <- allgerp_tib  %>% mutate(Type="all", Region="WholeGenome") %>%
             select(CHROM, POS, GERP, Type, Region)
 
-comb_tib=bind_rows(coding_tib, wg_tib)
+comb_tib=bind_rows(tidy_tab, tidy_full)
 
 #Add factors
-comb_tib$Type <- factor(comb_tib$Type, levels = c("synonymous","deleterious", "nonsense","all"))
-comb_tib$Region <- factor(comb_tib$Region, levels = c("Coding", "WholeGenome"))
-# Count mean - for this the plyr package is necessary!
-#require(plyr)
-#mean_gerp <- ddply(tidy_tab, "Category", summarise, grp.mean=mean(GERP))
-#mean_gerp
-#Make tibble with thresholds
-#thres<-tibble(Threshold=c("-9", "4", "5", "6", "8", "10", "13"), Pos=c(-9,4,5,6,8,10,13))
-#thres$Threshold <- factor(thres$Threshold, levels = c("-9", "4", "5", "6", "8", "10", "13"))
+comb_tib$Type <- factor(comb_tib$Type, levels = c("modifier", "synonymous","tolerated", "deleterious", "nonsense","all"))
+comb_tib$Region <- factor(comb_tib$Region, levels = c("Genic", "WholeGenome"))
 
 ################################################################################
 # PLOT
-# Output
-outfile=paste(plotdir, "FigureS4.pdf", sep="")
+outfile=paste(plotdir, "FigureS4.revised.pdf", sep="")
 
 p<-ggplot(data=comb_tib, aes(x=GERP, fill=Type)) +
     geom_density(alpha = 0.5, color=NA)+
     facet_wrap(~Region, scales="free") +
-    scale_fill_manual(values=c("lightblue", "dodgerblue4", "cadetblue4", "darkgrey")) +
+    scale_fill_manual(values=c("darkgrey", "lightblue", "seagreen4", "dodgerblue4","chocolate", "gray50")) +
     theme(panel.grid.major = element_line(colour = 'white'),
           panel.background = element_rect(fill = '#f5f4e6', colour = '#FFFDF8'),
           legend.box = "vertical",
